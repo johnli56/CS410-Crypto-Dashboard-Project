a0f9c92516a166b1df7643b13777599f61845a37
John Li <li.johnny34@gmail.com>
2020-02-27 18:29:41 -0800

    Multi-page navigation components

--- crypto-dashboard/src/App.js
+++ crypto-dashboard/src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter, Link, NavLink, Route, Switch } from "react-router-dom";
 import ReactDOM from "react-dom";
-import logo from "./logo.svg";
-import "./App.css";
+import bitcoinLogo from "./BTC_logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
@@ -9,62 +9,82 @@ import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 import { SocialIcon } from "react-social-icons";
+import Home from "./components/Home";
+import News from "./components/News";
+import Cryptocurrencies from "./components/Cryptocurrencies";
+import Contact from "./components/Contact";
+import About from "./components/About";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">CryptBoard</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link className="navbarLinks" href="#home">
-            Home
-          </Nav.Link>
-          <Nav.Link className="navbarLinks" href="#features">
-            News
-          </Nav.Link>
-          <Nav.Link className="navbarLinks" href="#pricing">
-            Pricing
-          </Nav.Link>
-          <Nav.Link className="navbarLinks" href="#contact">
-            Contact
-          </Nav.Link>
-          <Nav.Link className="navbarLinks" href="#about">
-            About us
-          </Nav.Link>
-          <Navbar.Brand href="#home" id="facebookLogo">
-            <SocialIcon network="facebook"></SocialIcon>
-          </Navbar.Brand>
-          <Navbar.Brand href="#home" id="instagramLogo">
-            <SocialIcon network="instagram"></SocialIcon>
-          </Navbar.Brand>
-          <Navbar.Brand href="#home" id="twitterLogo">
-            <SocialIcon network="twitter"></SocialIcon>
-          </Navbar.Brand>
-          <Navbar.Brand href="#home" id="linkedinLogo">
-            <SocialIcon network="linkedin"></SocialIcon>
-          </Navbar.Brand>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Find Crypto</Button>
-        </Form>
-      </Navbar>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Cryptocurrency Dashboard</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/home">CryptoBoard</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link as={NavLink} className="navbarLinks" to="/home">
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} className="navbarLinks" to="/news">
+              News
+            </Nav.Link>
+            <Nav.Link as={NavLink} className="navbarLinks" to="/pricing">
+              Cryptocurrencies
+            </Nav.Link>
+            <Nav.Link as={NavLink} className="navbarLinks" to="/contact">
+              Contact
+            </Nav.Link>
+            <Nav.Link as={NavLink} className="navbarLinks" to="/about">
+              About us
+            </Nav.Link>
+            <Navbar.Brand href="#home" id="facebookLogo">
+              <SocialIcon network="facebook"></SocialIcon>
+            </Navbar.Brand>
+            <Navbar.Brand href="#home" id="instagramLogo">
+              <SocialIcon network="instagram"></SocialIcon>
+            </Navbar.Brand>
+            <Navbar.Brand href="#home" id="twitterLogo">
+              <SocialIcon network="twitter"></SocialIcon>
+            </Navbar.Brand>
+            <Navbar.Brand href="#home" id="linkedinLogo">
+              <SocialIcon network="linkedin"></SocialIcon>
+            </Navbar.Brand>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Find Crypto</Button>
+            <Button className="loginButton" variant="outline-info">
+              Login
+            </Button>
+          </Form>
+        </Navbar>
+        <header className="App-header">
+          <img
+            src={bitcoinLogo}
+            className="App-logo"
+            width="50%"
+            height="50%"
+            alt="logo"
+            id="BTCLogo"
+          />
+          <p>Cryptocurrency Dashboard</p>
+          <p>By: John Li</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Route path="/home" component={Home} />
+        <Route path="/news" exact component={News} />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ crypto-dashboard/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const home = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>Login / Register page body content</p>
+    </div>
+  );
+};
+
+export default Login;
