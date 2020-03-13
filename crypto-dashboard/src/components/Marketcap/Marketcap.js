48056bef62f695e704f9a4d3c41697136c75a09a
Steven Schulze <stevenschulze@gmail.com>
2020-03-12 23:57:26 -0700

    Marketcap
    added pie chart for market cap

--- crypto-dashboard/src/components/Marketcap/Marketcap.js
+++ crypto-dashboard/src/components/Marketcap/Marketcap.js
@@ -1,5 +1,34 @@
 import React from "react";
 
+import {Pie} from 'react-chartjs-2';
+
+
+
+
+const data = {
+	labels: [
+		'Red',
+		'Blue',
+		'Yellow'
+	],
+	datasets: [{
+		data: [300, 50, 100],
+		backgroundColor: [
+		'#FF6384',
+		'#36A2EB',
+		'#FFCE56'
+		],
+		hoverBackgroundColor: [
+		'#FF6384',
+		'#36A2EB',
+		'#FFCE56'
+		]
+	}]
+};
+
+
+
+
 class Marketcap extends React.Component {
   constructor(props) {
     super(props);
@@ -10,6 +39,16 @@ class Marketcap extends React.Component {
       <div>
         <h1>Marketcap Page</h1>
         <p>Marketcap page body content</p>
+
+        <div>
+        <h2>Pie Example</h2>
+        <Pie data={data} />
+      </div>
+
+
+
+
+
       </div>
     );
   }
