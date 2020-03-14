96e222eb7c22f2a536dc8f63a7a676a88cfdba4a
johnli56 <li.johnny34@gmail.com>
2020-03-13 22:56:21 -0700

    Completed and Styled Bar graphs for Top 10 Cryptos
    Added more comments for charts.js

--- crypto-dashboard/src/components/Charts/Charts.js
+++ crypto-dashboard/src/components/Charts/Charts.js
@@ -11,15 +11,23 @@ class Charts extends React.Component {
       content: null,
       items: [],
       marketshareData: {
-        datasets: [{
-
-        }],
+        datasets: [{}],
         labels: []
       },
       tradeVolumeData: {
-        datasets: [{
-
-        }],
+        datasets: [{}],
+        labels: []
+      },
+      percentChange1HRData: {
+        datasets: [{}],
+        labels: []
+      },
+      percentChange24HRData: {
+        datasets: [{}],
+        labels: []
+      },
+      percentChange7DAYData: {
+        datasets: [{}],
         labels: []
       }
     };
@@ -53,7 +61,6 @@ class Charts extends React.Component {
           var topTenCryptocurrency1HRPercentChange = [];
           var topTenCryptocurrency7DAYPercentChange = [];
 
-
           var topTenCryptocurrencyColorArray = [
             "#fcba03",
             "#03b5fc",
@@ -65,7 +72,6 @@ class Charts extends React.Component {
             "#858585",
             "#F0D661",
             "#4879D5"
-
           ];
 
           // This for loop initializes the cryptocurrency array to the top 10 crypto names
@@ -82,23 +88,33 @@ class Charts extends React.Component {
             topTenCryptocurrencyMarketcapArray.push(
               this.state.items[i].quote.USD.market_cap
             );
-            topTenCryptocurrency24HRTradeVolume.push(this.state.items[i].quote.USD.volume_24h);
-            topTenCryptocurrency1HRPercentChange.push(this.state.items[i].quote.USD.percent_change_1h);
-            topTenCryptocurrency24HRPercentChange.push(this.state.items[i].quote.USD.percent_change_24h);
-            topTenCryptocurrency7DAYPercentChange.push(this.state.items[i].quote.USD.percent_change_7d);
+            topTenCryptocurrency24HRTradeVolume.push(
+              this.state.items[i].quote.USD.volume_24h
+            );
+            topTenCryptocurrency1HRPercentChange.push(
+              this.state.items[i].quote.USD.percent_change_1h
+            );
+            topTenCryptocurrency24HRPercentChange.push(
+              this.state.items[i].quote.USD.percent_change_24h
+            );
+            topTenCryptocurrency7DAYPercentChange.push(
+              this.state.items[i].quote.USD.percent_change_7d
+            );
           }
 
-          // DEBUG: Testing results
+          // DEBUG: Testing if we are fetching the correct data and storing them
           //console.log(result);
           console.log(result.data);
           console.log(topTenCryptocurrencyArray);
           console.log(topTenCryptocurrencyMarketcapArray);
           console.log(topTenCryptocurrency24HRTradeVolume);
+          console.log(topTenCryptocurrency1HRPercentChange);
+          console.log(topTenCryptocurrency24HRPercentChange);
+          console.log(topTenCryptocurrency7DAYPercentChange);
           console.log("before the setState" + this.state.simpleData);
 
           // Initialize the local component state variables with the data gathered
           this.setState({
-
             marketshareData: {
               labels: topTenCryptocurrencyArray,
               datasets: [
@@ -108,7 +124,7 @@ class Charts extends React.Component {
                 }
               ]
             },
-            
+
             tradeVolumeData: {
               labels: topTenCryptocurrencyArray,
               datasets: [
@@ -117,13 +133,46 @@ class Charts extends React.Component {
                   backgroundColor: topTenCryptocurrencyColorArray
                 }
               ]
+            },
+
+            percentChange1HRData: {
+              labels: topTenCryptocurrencyArray,
+              datasets: [
+                {
+                  data: topTenCryptocurrency1HRPercentChange,
+                  backgroundColor: topTenCryptocurrencyColorArray
+                }
+              ]
+            },
+
+            percentChange24HRData: {
+              labels: topTenCryptocurrencyArray,
+              datasets: [
+                {
+                  data: topTenCryptocurrency24HRPercentChange,
+                  backgroundColor: topTenCryptocurrencyColorArray
+                }
+              ]
+            },
+
+            percentChange7DAYData: {
+              labels: topTenCryptocurrencyArray,
+              datasets: [
+                {
+                  data: topTenCryptocurrency7DAYPercentChange,
+                  backgroundColor: topTenCryptocurrencyColorArray
+                }
+              ]
             }
           });
 
           // Replace with the new data, double check it's what we want it to be.
           console.log(this.state.marketshareData);
-          console.log("Trade Volume data below: ");
+          console.log("Trade Volume data and other data below: ");
           console.log(this.state.tradeVolumeData);
+          console.log(this.state.percentChange1HRData);
+          console.log(this.state.percentChange24HRData);
+          console.log(this.state.percentChange7DAYData);
         },
         // Handle errors here so that we do not swallow exceptions from component bug
         error => {
@@ -136,37 +185,63 @@ class Charts extends React.Component {
   }
 
   render() {
+    // TODO: Make a button where it can display a minimized "grid-like form" of the graphs/charts
+    // and a "expanded enlarged view" where the graphs can be more closely examined.
     return (
       <div className="ChartsPage">
         <h1>Cryptocurrency Charts</h1>
         <p>
-          Charts display Marketshare, Trade volume, Prices, and Percent Change
-          of Price.
+          Charts display Top 10 Cryptocurrency's Marketshare, Trade volume, and
+          Percent Change of Price.
         </p>
 
         <div className="MarketSharePieChart">
-          <h2>Pie Chart of top 10 Cryptocurrency's Marketshare in USD ( $ )</h2>
+          <h2>Pie Chart of top 10 Cryptocurrency Marketshare in USD ( $ )</h2>
           <Pie id="top10MarketcapPieChart" data={this.state.marketshareData} />
         </div>
 
         <div className="TradeVolume24HRBarGraph">
-          <h2>Bar Graph of top 10 Cryptocurrency's 24 Hour Trade Volume in USD ( $ )</h2>
-          <Bar id="top10TradeVolume24HRBarGraph" data={this.state.tradeVolumeData} />
+          <h2>
+            Bar Graph of top 10 Cryptocurrency 24 Hour Trade Volume in USD ( $ )
+          </h2>
+          <Bar
+            id="top10TradeVolume24HRBarGraph"
+            data={this.state.tradeVolumeData}
+          >
+            hi
+          </Bar>
         </div>
 
         <div className="PercentChange1HRLineGraph">
-        <h2>Line Graph of top 10 Cryptocurrency's 1 Hour Price Percent Change in USD ( $ )</h2>
-
+          <h2>
+            Line Graph of top 10 Cryptocurrency 1 Hour Price % Change in USD ( $
+            )
+          </h2>
+          <Bar
+            id="top10PercentChange1HRBarGraph"
+            data={this.state.percentChange1HRData}
+          ></Bar>
         </div>
 
         <div className="PercentChange24HRLineGraph">
-        <h2>Line Graph of top 10 Cryptocurrency's 24 Hour Price Percent Change in USD ( $ )</h2>
-          
+          <h2>
+            Line Graph of top 10 Cryptocurrency 24 Hour Price % Change in USD (
+            $ )
+          </h2>
+          <Bar
+            id="top10PercentChange24HRBarGraph"
+            data={this.state.percentChange24HRData}
+          ></Bar>
         </div>
 
-        <div className="PercentChange7DAYLineGraph">
-        <h2>Line Graph of top 10 Cryptocurrency's 7 Day Price Percent Change in USD ( $ )</h2>
-          
+        <div className="PercentChange7DAYBarGraph">
+          <h2>
+            Bar Graph of top 10 Cryptocurrency 7 Day Price % Change in USD ( $ )
+          </h2>
+          <Bar
+            id="top10PercentChange7DAYBarGraph"
+            data={this.state.percentChange7DAYData}
+          ></Bar>
         </div>
       </div>
     );
